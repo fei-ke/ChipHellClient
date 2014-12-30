67a0ea94e4b75fdae3bb85a72dd80e5e23ac9c8d
fei-ke <jinyang656@qq.com>
2014-12-30 10:38:00 +0800

    little fix

--- chh/src/main/java/com/fei_ke/chiphellclient/utils/UrlParamsMap.java
+++ chh/src/main/java/com/fei_ke/chiphellclient/utils/UrlParamsMap.java
@@ -1,7 +1,6 @@
 package com.fei_ke.chiphellclient.utils;
 
 import java.util.HashMap;
-import java.util.Map;
 
 /**
  * 将url中的请求参数转换为map
@@ -18,11 +17,10 @@ public class UrlParamsMap extends HashMap<String, String> {
         url = url.substring(s + 1);
 
         String[] params = url.split("&");
-        Map<String, String> map = new HashMap<String, String>();
         for (String paramGroup : params) {
             String[] param = paramGroup.split("=");
             String key = param[0];
-            String value = null;
+            String value;
             if (param.length > 1) {
                 value = param[1];
             } else {
