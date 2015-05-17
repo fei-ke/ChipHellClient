29e1457ced6050fc06653ca6751e64fbaace6d70
fei-ke <jinyang656@qq.com>
2015-05-17 18:05:55 +0800

    使用圆形头像,首次加载动画

--- chh/src/main/java/com/fei_ke/chiphellclient/ui/customviews/PostItemView.java
+++ chh/src/main/java/com/fei_ke/chiphellclient/ui/customviews/PostItemView.java
@@ -2,10 +2,12 @@
 package com.fei_ke.chiphellclient.ui.customviews;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.text.Html;
 import android.text.Html.ImageGetter;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -15,13 +17,20 @@ import com.fei_ke.chiphellclient.bean.Post;
 import com.fei_ke.chiphellclient.constant.Constants;
 import com.fei_ke.chiphellclient.utils.LogMessage;
 import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
+import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
 
 import org.androidannotations.annotations.EViewGroup;
 import org.androidannotations.annotations.ViewById;
 
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * 帖子内的一个item
- * 
+ *
  * @author fei-ke
  * @2014-6-16
  */
@@ -35,6 +44,8 @@ public class PostItemView extends FrameLayout {
 
     @ViewById(R.id.textView_authi)
     TextView textViewAuthi;
+
+    private static ImageLoadingListener animateFirstListener = new AnimateFirstDisplayListener();
 
     public static PostItemView newInstance(Context context) {
         return PostItemView_.build(context);
@@ -53,7 +64,7 @@ public class PostItemView extends FrameLayout {
     }
 
     public void bindValue(Post post) {
-        ImageLoader.getInstance().displayImage(post.getAvatarUrl(), imageViewAvatar,Constants.avatarDisplayOption);
+        ImageLoader.getInstance().displayImage(post.getAvatarUrl(), imageViewAvatar, Constants.avatarDisplayOption, animateFirstListener);
         textViewAuthi.setText(Html.fromHtml(post.getAuthi()));
         String content = post.getContent();
         if (post.getImgList() != null) {
@@ -72,4 +83,20 @@ public class PostItemView extends FrameLayout {
         }, null));
     }
 
+    private static class AnimateFirstDisplayListener extends SimpleImageLoadingListener {
+
+        static final List<String> displayedImages = Collections.synchronizedList(new LinkedList<String>());
+
+        @Override
+        public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+            if (loadedImage != null) {
+                ImageView imageView = (ImageView) view;
+                boolean firstDisplay = !displayedImages.contains(imageUri);
+                if (firstDisplay) {
+                    FadeInBitmapDisplayer.animate(imageView,600);
+                    displayedImages.add(imageUri);
+                }
+            }
+        }
+    }
 }
